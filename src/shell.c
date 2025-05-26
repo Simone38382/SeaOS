3d85c79e22c4aa21482510e334caf476cbbb21d0
Simone38382 <pon.emma21@gmail.com>
2025-05-26 12:19:21 +0000

    fix strncmp and add unix-like shell and filesystem

--- src/shell.c
+++ src/shell.c
@@ -1,6 +1,7 @@
 // shell.c
 #include "shell.h"
 #include <string.h>
+#include <stddef.h>
 
 #define VIDEO_MEMORY (char*)0xb8000
 #define MAX_INPUT 80
@@ -33,14 +34,76 @@ void clear_screen() {
 }
 
 void print_prompt() {
-    print("SeaOS> ");
+    print("user@seaos:~$ ");
+}
+
+// --- Simulazione file system Unix-like ---
+typedef struct File {
+    const char* name;
+    const char* content;
+} File;
+
+typedef struct Directory {
+    const char* name;
+    File* files;
+    int file_count;
+} Directory;
+
+// File di esempio
+File root_files[] = {
+    {"README.txt", "Benvenuto su SeaOS!\nQuesto è un file di esempio."},
+    {"hello.txt", "Hello from SeaOS!"}
+};
+File etc_files[] = {
+    {"os-release", "NAME=SeaOS\nVERSION=0.1"}
+};
+
+Directory fs[] = {
+    {"/", root_files, 2},
+    {"/etc", etc_files, 1}
+};
+const int dir_count = 2;
+
+// Funzione per ls simulato
+void ls_sim(const char* path) {
+    for (int i = 0; i < dir_count; i++) {
+        if (!strcmp(path, fs[i].name)) {
+            for (int j = 0; j < fs[i].file_count; j++) {
+                print(fs[i].files[j].name);
+                print("  ");
+            }
+            print("\n");
+            return;
+        }
+    }
+    print("ls: directory non trovata\n");
+}
+
+// Funzione per cat simulato
+void cat_sim(const char* path, const char* filename) {
+    for (int i = 0; i < dir_count; i++) {
+        if (!strcmp(path, fs[i].name)) {
+            for (int j = 0; j < fs[i].file_count; j++) {
+                if (!strcmp(filename, fs[i].files[j].name)) {
+                    print(fs[i].files[j].content);
+                    print("\n");
+                    return;
+                }
+            }
+            print("cat: file non trovato\n");
+            return;
+        }
+    }
+    print("cat: directory non trovata\n");
 }
 
 void shell() {
     char input[MAX_INPUT];
     int pos;
     clear_screen();
-    print("SeaOS Shell. Digita 'help' per i comandi.\n");
+    print("SeaOS (Unix-like) [Version 0.1]\n");
+    print("(C) 2025 SeaOS Team. All rights reserved.\n");
+    print("Type 'help' to list commands.\n\n");
     while (1) {
         print_prompt();
         pos = 0;
@@ -82,9 +145,26 @@ void shell() {
         // Comandi
         if (input[0] == '\0') continue;
         if (!strcmp(input, "help")) {
-            print("Comandi disponibili: help, clear\n");
+            print("Comandi disponibili: help, clear, echo, ls, cat, uname\n");
         } else if (!strcmp(input, "clear")) {
             clear_screen();
+        } else if (!strcmp(input, "ls")) {
+            ls_sim("/");
+        } else if (!strncmp(input, "ls /etc", 7)) {
+            ls_sim("/etc");
+        } else if (!strcmp(input, "uname")) {
+            print("SeaOS 0.1 (unix-like)\n");
+        } else if (!strcmp(input, "cat")) {
+            print("cat: manca il file da leggere\n");
+        } else if (!strcmp(input, "echo")) {
+            print("\n");
+        } else if (!strncmp(input, "echo ", 5)) {
+            print(input + 5);
+            print("\n");
+        } else if (!strncmp(input, "cat /etc/", 9)) {
+            cat_sim("/etc", input + 9);
+        } else if (!strncmp(input, "cat ", 4)) {
+            cat_sim("/", input + 4);
         } else {
             print("Comando non riconosciuto\n");
         }
@@ -97,6 +177,17 @@ int strcmp(const char *s1, const char *s2) {
         s1++;
         s2++;
     }
+    return *(const unsigned char*)s1 - *(const unsigned char*)s2;
+}
+
+// Semplice strncmp per ambienti freestanding
+int strncmp(const char *s1, const char *s2, size_t n) {
+    while (n && *s1 && (*s1 == *s2)) {
+        ++s1;
+        ++s2;
+        --n;
+    }
+    if (n == 0) return 0;
     return *(const unsigned char*)s1 - *(const unsigned char*)s2;
 }
 
